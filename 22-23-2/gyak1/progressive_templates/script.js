a06fadf125866e8b3f8d8b66ce57c640c099c4a8
Vikor Bogdán Vitold <56073217+vikorbogdan@users.noreply.github.com>
2023-03-04 08:36:20 +0100

    gyak_1

---
+++ 22-23-2/gyak1/progressive_templates/script.js
@@ -0,0 +1,40 @@
+// Landing page – navigáció rögzítése Ha elgördült az oldal 200px-nyit, akkor alkalmazzuk a navbar-scrolled stílusosztályt a nav elemen. Ügyelj arra, hogy a scroll esemény nagyon sokszor hívódik meg!
+
+const nav = document.querySelector("nav");
+let waiting;
+
+// const onScroll = (e) => {
+//   //   if (window.scrollY > 200) {
+//   //     nav.classList.add("navbar-scrolled");
+//   //   } else {
+//   //     nav.classList.remove("navbar-scrolled");
+//   //   }
+//   if (!waiting) {
+//     nav.classList.toggle("navbar-scrolled", window.scrollY > 200);
+//     waiting = true;
+//     setTimeout(() => (waiting = false), 10);
+//   }
+// };
+
+const onScroll = (e) => {
+  nav.classList.toggle("navbar-scrolled", window.scrollY > 200);
+};
+
+window.addEventListener("scroll", _.throttle(onScroll, 100));
+
+// Landing page – animáció megjelenéskor Ha egy elem gördítés közben a viewportba ér, akkor valamilyen animáció segítségével jelenjen meg! Az elemeket deklaratívan jelöljük meg HTML5 data attribútumokat használva, pl. data-scroll. Az animáció nevét is eltárolhatod data attribútumban, pl. data-scroll-animation="fadeInUp". Animációhoz használhatod az animate.css könyvtárat. Ügyelj arra, hogy a scroll esemény nagyon sokszor hívódik meg!
+
+const onObserve = (entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add(
+        "animate__animated",
+        `animate__${entry.target.dataset.scrollAnimation}`
+      );
+    }
+  });
+};
+
+const observer = new IntersectionObserver(onObserve, { threshold: 1.0 });
+
+document.querySelectorAll("[data-scroll]").forEach((e) => observer.observe(e));
