01fc7e17cfd47a2b086c4a877034e0b4d610f216
Vikor Bogdán <vikorbogdan@gmail.com>
2024-02-12 19:11:14 +0100

    1. gyakorlat

---
+++ 23-24-2/3_csoport/1_gyakorlat/landing_page/script.js
@@ -0,0 +1,51 @@
+const navBar = document.querySelector("nav#mainNav");
+
+const onScroll = () => {
+  //   const hasScrolledEnough = window.scrollY > 200;
+  //   if (hasScrolledEnough) {
+  //     navBar.classList.add("navbar-scrolled");
+  //   } else {
+  //     navBar.classList.remove("navbar-scrolled");
+  //   }
+  navBar.classList.toggle("navbar-scrolled", window.scrollY > 200);
+};
+
+window.addEventListener("scroll", _.throttle(onScroll, 200));
+
+// 3. feladat
+
+const elementsToAnimate = document.querySelectorAll("[data-scroll]");
+// b. Hozzuk létre a callback functiont
+const onAnimationObserve = (entries) => {
+  entries
+    .filter((entry) => entry.isIntersecting)
+    .forEach((entry) => {
+      entry.target.classList.add(
+        "animate__animated",
+        `animate__${entry.target.dataset.scrollAnimation}`
+      );
+    });
+};
+
+// a. Példányosítsuk az Observert
+const animationObserver = new IntersectionObserver(onAnimationObserve, {
+  threshold: 1.0,
+});
+
+// c. Rendeljük hozzá elemekhez az Observert
+elementsToAnimate.forEach((elem) => animationObserver.observe(elem));
+
+// 4. feladat
+const progressBar = document.querySelector("#scrollProgress");
+
+const updateProgressBar = () => {
+  const scroll = document.documentElement.scrollTop;
+  const height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+  const progressPercentage = Math.round((scroll / height) * 100);
+  progressBar.value = progressPercentage;
+};
+
+window.addEventListener("scroll", _.throttle(updateProgressBar, 20));
