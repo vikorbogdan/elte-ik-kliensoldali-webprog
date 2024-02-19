82c109257cef7b79abbd88b8203e4e73925307f1
Vikor Bogdán <vikorbogdan@gmail.com>
2024-02-19 17:01:03 +0100

    függvény egységbezárás

--- 23-24-2/3_csoport/2_gyakorlat/script.js
+++ 23-24-2/3_csoport/2_gyakorlat/script.js
@@ -1,52 +1,18 @@
-// Elemek kiválasztása
-const tableElement = document.querySelector("table");
-const tableBodyElement = tableElement.querySelector("tbody");
-const tableHeaderElement = tableElement.querySelector("thead");
+const sortableTable = (tableId) => {
+  // Elemek kiválasztása
+  const tableElement = document.querySelector(`#${tableId}`);
+  const tableBodyElement = tableElement.querySelector("tbody");
+  const tableHeaderElement = tableElement.querySelector("thead");
 
-// Táblázat adatainak kiolvasása
-/*
-[
-    [Komment, Kép],
-    [Visegrádi csata, Járó],
-    [Egri vár ostroma, Ablak]
-]
-*/
-
-const tableBodyData = [...tableBodyElement.querySelectorAll("tr")].map(
-  (trElement) =>
-    [...trElement.querySelectorAll("td")].map(
-      (tdElement) => tdElement.innerText
-    )
-);
-
-tableBodyData.sort((a, b) => a[0].localeCompare(b[0]));
-
-tableBodyElement.innerHTML = tableBodyData
-  .map(
-    (row) => `
-    <tr>
-        ${row
-          .map(
-            (cell) => `
-        <td>
-            ${cell}
-        </td>
-        `
-          )
-          .join("")}
-    </tr>
-`
-  )
-  .join("");
-
-tableHeaderElement.style.cursor = "pointer";
-
-tableHeaderElement.addEventListener("click", (event) => {
-  const headerCellElement = event.target.closest("th");
-  const headerCellIndex = headerCellElement.cellIndex;
-  tableBodyData.sort((a, b) =>
-    a[headerCellIndex].localeCompare(b[headerCellIndex])
+  const tableBodyData = [...tableBodyElement.querySelectorAll("tr")].map(
+    (trElement) =>
+      [...trElement.querySelectorAll("td")].map(
+        (tdElement) => tdElement.innerText
+      )
   );
+
+  tableBodyData.sort((a, b) => a[0].localeCompare(b[0]));
+
   tableBodyElement.innerHTML = tableBodyData
     .map(
       (row) => `
@@ -64,6 +30,34 @@ tableHeaderElement.addEventListener("click", (event) => {
 `
     )
     .join("");
-});
 
-console.log(tableBodyData);
+  tableHeaderElement.style.cursor = "pointer";
+
+  tableHeaderElement.addEventListener("click", (event) => {
+    const headerCellElement = event.target.closest("th");
+    const headerCellIndex = headerCellElement.cellIndex;
+    tableBodyData.sort((a, b) =>
+      a[headerCellIndex].localeCompare(b[headerCellIndex])
+    );
+    tableBodyElement.innerHTML = tableBodyData
+      .map(
+        (row) => `
+    <tr>
+        ${row
+          .map(
+            (cell) => `
+        <td>
+            ${cell}
+        </td>
+        `
+          )
+          .join("")}
+    </tr>
+`
+      )
+      .join("");
+  });
+};
+
+sortableTable("table1");
+sortableTable("table2");
