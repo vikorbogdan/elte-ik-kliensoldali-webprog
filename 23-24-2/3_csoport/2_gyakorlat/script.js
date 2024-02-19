5a025351977dd6b787b8e06a4298816442e8b3b9
Vikor Bogdán <vikorbogdan@gmail.com>
2024-02-19 17:33:09 +0100

    egységbezárás osztály segítségével

--- 23-24-2/3_csoport/2_gyakorlat/script.js
+++ 23-24-2/3_csoport/2_gyakorlat/script.js
@@ -1,63 +1,51 @@
-const sortableTable = (tableId) => {
-  // Elemek kiválasztása
-  const tableElement = document.querySelector(`#${tableId}`);
-  const tableBodyElement = tableElement.querySelector("tbody");
-  const tableHeaderElement = tableElement.querySelector("thead");
-
-  const tableBodyData = [...tableBodyElement.querySelectorAll("tr")].map(
-    (trElement) =>
-      [...trElement.querySelectorAll("td")].map(
-        (tdElement) => tdElement.innerText
-      )
-  );
-
-  tableBodyData.sort((a, b) => a[0].localeCompare(b[0]));
-
-  tableBodyElement.innerHTML = tableBodyData
-    .map(
-      (row) => `
-    <tr>
-        ${row
-          .map(
-            (cell) => `
-        <td>
-            ${cell}
-        </td>
-        `
-          )
-          .join("")}
-    </tr>
-`
-    )
-    .join("");
-
-  tableHeaderElement.style.cursor = "pointer";
-
-  tableHeaderElement.addEventListener("click", (event) => {
-    const headerCellElement = event.target.closest("th");
-    const headerCellIndex = headerCellElement.cellIndex;
-    tableBodyData.sort((a, b) =>
-      a[headerCellIndex].localeCompare(b[headerCellIndex])
+class SortableTable {
+  constructor(tableId) {
+    this.tableElement = document.querySelector(`#${tableId}`);
+    this.tableBodyElement = this.tableElement.querySelector("tbody");
+    this.tableHeaderElement = this.tableElement.querySelector("thead");
+    this.tableBodyData = [...this.tableBodyElement.querySelectorAll("tr")].map(
+      (trElement) =>
+        [...trElement.querySelectorAll("td")].map(
+          (tdElement) => tdElement.innerText
+        )
     );
-    tableBodyElement.innerHTML = tableBodyData
+
+    this.sortTableBody(0);
+    this.tableBodyElement.innerHTML = this.renderTableBody();
+
+    this.tableHeaderElement.style.cursor = "pointer";
+
+    this.tableHeaderElement.addEventListener("click", (event) => {
+      const headerCellElement = event.target.closest("th");
+      const headerCellIndex = headerCellElement.cellIndex;
+      this.tableBodyData.sort(this.sortTableBody(headerCellIndex));
+      this.tableBodyElement.innerHTML = this.renderTableBody();
+    });
+  }
+
+  sortTableBody = (cellIndex) => {
+    return (a, b) => a[cellIndex].localeCompare(b[cellIndex]);
+  };
+  renderTableBody = () => {
+    return this.tableBodyData
       .map(
         (row) => `
-    <tr>
-        ${row
-          .map(
-            (cell) => `
-        <td>
-            ${cell}
-        </td>
-        `
-          )
-          .join("")}
-    </tr>
-`
+     <tr>
+         ${row
+           .map(
+             (cell) => `
+         <td>
+             ${cell}
+         </td>
+         `
+           )
+           .join("")}
+     </tr>
+  `
       )
       .join("");
-  });
-};
+  };
+}
 
-sortableTable("table1");
-sortableTable("table2");
+new SortableTable("table1");
+new SortableTable("table2");
