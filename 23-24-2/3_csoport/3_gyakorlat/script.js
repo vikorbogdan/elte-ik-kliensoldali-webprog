638b6b884e3f6266346ac5ac136e1b90b11ccfa6
Vikor Bogdán <vikorbogdan@gmail.com>
2024-02-26 16:38:02 +0100

    Added carousel functionality with interactive buttons

--- 23-24-2/3_csoport/3_gyakorlat/script.js
+++ 23-24-2/3_csoport/3_gyakorlat/script.js
@@ -1,2 +1,39 @@
 const carouselElement = document.querySelector("#carousel");
 const imageElements = carouselElement.querySelectorAll("img");
+
+let activeIndex = 0;
+
+// Egy olyan függvény, ami activeIndex alapján jeleníti meg a helyes képet
+const render = () => {
+  imageElements.forEach((image, index) => {
+    image.classList.toggle("active", index === activeIndex);
+  });
+};
+
+// setInterval(() => {
+//   activeIndex = (activeIndex + 1) % imageElements.length;
+//   render();
+// }, 500);
+
+const previousButton = document.createElement("button");
+const nextButton = document.createElement("button");
+
+previousButton.innerText = "Previous";
+nextButton.innerText = "Next";
+
+carouselElement.appendChild(previousButton);
+carouselElement.appendChild(nextButton);
+
+const previousImageButtonHandler = () => {
+  activeIndex = (activeIndex - 1 + imageElements.length) % imageElements.length;
+  render();
+};
+const nextImageButtonHandler = () => {
+  activeIndex = (activeIndex + 1) % imageElements.length;
+  render();
+};
+
+previousButton.addEventListener("click", previousImageButtonHandler);
+nextButton.addEventListener("click", nextImageButtonHandler);
+
+render();
