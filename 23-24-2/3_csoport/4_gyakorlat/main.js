928a6e30b75a53b61885076a560c8ec8f28583f8
Vikor Bogdán <vikorbogdan@gmail.com>
2024-03-04 17:28:12 +0100

    cowsay!!!!

---
+++ 23-24-2/3_csoport/4_gyakorlat/main.js
@@ -0,0 +1,13 @@
+import { mul, add } from "./matek.js";
+import cowsay from "cowsay";
+
+console.log(add(3, 3));
+console.log(mul(3, 3));
+console.log(
+  cowsay.say({
+    text: "de jouououou volt a zh!!!",
+    e: "^^",
+    T: "# ",
+    r: true,
+  })
+);
