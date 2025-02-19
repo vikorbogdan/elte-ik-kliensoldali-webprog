ab1f0599748dfe4f37a1e1a44229323eb4baab28
Vikor Bogdan <vikorbogdan@gmail.com>
2025-02-19 20:47:44 +0100

    osztályalapú egységbezárás

--- 24-25-2/3_csoport/2_gyakorlat/script.js
+++ 24-25-2/3_csoport/2_gyakorlat/script.js
@@ -1,22 +1,25 @@
-function sortableTable(elementId){
-const tableElement = document.querySelector(`#${elementId}`);
-const tableBodyElement = tableElement.querySelector("tbody");
-const tableHeaderElement = tableElement.querySelector("thead");
+class SortableTable {
+    constructor(tableElement){
+        this.tableBodyElement = tableElement.querySelector("tbody");
+        this.tableHeaderElement = tableElement.querySelector("thead");
+        
+        this.tableData = [...this.tableBodyElement.querySelectorAll("tr")]
+        .map(row => [...row.querySelectorAll("td")]
+        .map(cell => cell.innerText))
+        
+        this.tableHeaderElement.style.cursor = "pointer";
+        this.tableHeaderElement.addEventListener("click",this.onHeaderClick)
+    }
 
-const tableData = [...tableBodyElement.querySelectorAll("tr")]
-.map(row => [...row.querySelectorAll("td")]
-.map(cell => cell.innerText))
 
-tableHeaderElement.style.cursor = "pointer";
-
-function onHeaderClick(event) {
+onHeaderClick = (event) => {
     const headerCellElement = event.target.closest("th");
     const headerCellIndex = headerCellElement.cellIndex
 
-    tableData.sort((a,b) => a[headerCellIndex].localeCompare(b[headerCellIndex]));
+    this.tableData.sort((a,b) => a[headerCellIndex].localeCompare(b[headerCellIndex]));
 
-    tableBodyElement.innerHTML = `
-        ${tableData.map(row => `
+    this.tableBodyElement.innerHTML = `
+        ${this.tableData.map(row => `
             <tr>
                 ${row.map(cell => `
                     <td>
@@ -27,10 +30,7 @@ function onHeaderClick(event) {
             `).join("")}
     `
 }
-
-tableHeaderElement.addEventListener("click",onHeaderClick)
-
 }
 
-sortableTable("elso")
-sortableTable("masodik")
+const table1 = new SortableTable(document.querySelector("#elso"))
+const table2 = new SortableTable(document.querySelector("#masodik"))
