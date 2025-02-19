20d454ba6f4b69eccdd249be6773660d549d0378
Vikor Bogdan <vikorbogdan@gmail.com>
2025-02-19 18:54:40 +0100

    alap megoldás

---
+++ 24-25-2/4_csoport/2_gyakorlat/script.js
@@ -0,0 +1,27 @@
+const tableElement = document.querySelector("table");
+const tableBodyElement = tableElement.querySelector("tbody");
+const tableHeaderElement = tableElement.querySelector("thead")
+
+const tableData = [...tableBodyElement.querySelectorAll("tr")].map(tr => [...tr.querySelectorAll("td")].map(td => td.innerText))
+
+tableHeaderElement.style.cursor = "pointer";
+
+
+function onHeaderClick(event){
+    const headerCellElement = event.target.closest("th")
+
+    tableData.sort((a,b) => a[headerCellElement.cellIndex].localeCompare(b[headerCellElement.cellIndex]))
+tableBodyElement.innerHTML = tableData.map(row => `
+    <tr>
+        ${row.map(cell => `
+            <td>
+                ${cell}
+            </td>
+            `).join("")}
+    </tr>
+`).join("")
+
+}
+
+
+tableHeaderElement.addEventListener("click",onHeaderClick);
